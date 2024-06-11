d098d878def3b30d3f7b7c45ba9399517b33493b
MTCodes01 <koshi9866@gmail.com>
2024-06-11 22:02:02 +0530

    update v1.3

--- Module/Algorithms.py
+++ Module/Algorithms.py
@@ -1,10 +1,9 @@
 def Bubble(lst):
-    print(lst)
     for i in range(len(lst)):
         for j in range(0,len(lst)-i-1):
-            print(i,j)
             if lst[j] > lst[j+1]:
                 lst[j], lst[j + 1] = lst[j + 1], lst[j]
+            print(i, j)
     return lst
         
 
@@ -14,18 +13,19 @@ def Selection(lst):
         for j in range(i+1, len(lst)):
             if lst[temp] > lst[j]:
                 temp = j
+            print(i, j)
         if i != temp:
             lst[i], lst[temp] = lst[temp], lst[i]
     return lst
 
 def Insertion(lst):
-    print(lst)
     for i in range(1, len(lst)):
-        last = lst[i-1]
-        for j in range(i, len(lst)):
-            print(i,j)
-            continue
-    print(lst)
+        index = i
+        for j in range(i-1, -1, -1):
+            if lst[index] < lst[j]:
+                lst[index], lst[j] = lst[j], lst[index]
+                index -= index-j
+            print(i, j)
     return lst
 
 def Merge(lst):
